6442b62e3616cce8eee790520e7371e80b84805b
Himanshu Joshi <himharsh1997@gmail.com>
2019-04-07 22:47:16 +0530

    Half almost complete

--- src/app/myservice.service.ts
+++ src/app/myservice.service.ts
@@ -7,12 +7,16 @@ import {Step1data} from './step1data';
 })
 export class MyserviceService {
 private _postsURL = "http://private-8d2fe-assignment36.apiary-mock.com/employees";
+private vendor = "https://polls.apiblueprint.org/vendors";
   value1;
   value2 = "";
 
   constructor(private http: HttpClient) { }
   getdata() {
     return this.http.get(this._postsURL);
+  }
+  getvendor(){
+    return this.http.get(this.vendor);
   }
   steps1_data = {'date':"",'shift':"",'t_type':'Pick-up','a_type':'Nodal'};
   steps2_data;
--- src/app/step3/step3.component.ts
+++ src/app/step3/step3.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MyserviceService } from '../myservice.service';
 @Component({
   selector: 'app-step3',
   templateUrl: './step3.component.html',
-  styleUrls: ['./step3.component.scss']
+  styleUrls: ['./step3.component.scss'],
+  providers:[MyserviceService]
 })
 export class Step3Component implements OnInit {
 
-  constructor() { }
+  constructor(private api:MyserviceService) {
+   }
+ mylist;
 
   ngOnInit() {
+    this.api.getvendor().subscribe((res)=>{
+     console.log(res);
+     this.mylist = res;
+       });
   }
 
 }
